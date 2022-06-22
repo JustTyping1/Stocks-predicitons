f641b329931cdf87af785b59112a7751e8f68b05
JustTyping1 <aditya.chorghade@outlook.com>
2022-06-22 20:20:42 +0100

    changed model

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
+from cProfile import label
+from turtle import color
+from click import command
 import pandas as pd
 from sklearn.tree import *
 from sklearn.linear_model import *
-import matplotlib as plt
+import tkinter as tk
 
-def predict(open, high, low):
+window = tk.Tk()
+
+def dowpredict():
 
     data = pd.read_csv('HistoricalPrices.csv')
 
     X = data.drop(columns=['Date'])
     del X[' Close']
+    del X[' High']
+    del X[' Low']
 
     Y = data[' Close']
 
-    model = DecisionTreeClassifier()
+    model = LinearRegression()
     model.fit(X, Y)
-    predictions = model.predict([ [open, high, low] ])
-    print(predictions)
+    predictions = model.predict([[ int(dowentry.get()) ], [int(dowentry.get())]])
+    ansdow = tk.Label(text = "I PREDICT... \n" + str(predictions[1]))
+    ansdow.pack()
 
-predict(20000, 30000, 20000)
-        
+intro = tk.Label(text = "ENTER OPEN PRICE >")
+intro.pack()
+global dowentry
+dowentry = tk.Entry()
+dowpredictbutton = tk.Button(text = "Predict!", command = dowpredict)
+dowentry.pack()
+dowpredictbutton.pack()
+
+window.mainloop()
